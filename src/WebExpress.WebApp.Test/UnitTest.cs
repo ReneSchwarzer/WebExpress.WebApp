d4a63246325a4c99b3591b170dbcd0fa05e90e5c
ReneSchwarzer <rene_schwarzer@hotmail.de>
2023-12-10 10:41:35 +0100

    bug fixing

---
+++ src/WebExpress.WebApp.Test/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using WebExpress.WebCore;
+using Xunit.Abstractions;
+
+namespace WebExpress.WebApp.Test
+{
+    public class UnitTest
+    {
+        public ITestOutputHelper Output { get; private set; }
+
+        public UnitTest(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        [Fact]
+        public void StartServer()
+        {
+            //Process.Start("http://localhost/");
+
+            WebEx.Main([]);
+        }
+    }
+}
