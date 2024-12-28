07761021c92b6604d1eaf07d2dfc5fdfa53811ea
Rene Schwarzer <rene_schwarzer@hotmail.de>
2024-12-28 19:53:53 +0100

    add system plugin functionality

---
+++ src/WebExpress.WebApp/WebExWebApp.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using WebExpress.WebCore.WebAttribute;
+
+[assembly: SystemPlugin()]
+[assembly: InternalsVisibleTo("WebExpress.WebUI.Test")]
+
+namespace WebExpress.WebApp
+{
+    /// <summary>
+    /// Represents the WebExWebApp class which is a part of the WebExpress.WebWebApp namespace.
+    /// This class is internal and is intended for use within the WebExpress.WebApp assembly.
+    /// </summary>
+    internal class WebExWebApp
+    {
+    }
+}
